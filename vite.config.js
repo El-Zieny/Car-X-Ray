ab4d5731f22858ba1fd2143ad05d194a317b0420
AbdUlRahman <abdoelzieny72@gmail.com>
2026-06-18 17:41:28 +0300

    rr

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
+import fs from "fs";
+
+const htmlFiles = {};
+
+fs.readdirSync(__dirname)
+  .filter((file) => file.endsWith(".html"))
+  .forEach((file) => {
+    const name = file.replace(".html", "");
+    htmlFiles[name] = resolve(__dirname, file);
+  });
 
 export default defineConfig({
-  plugins: [
+    plugins: [
     tailwindcss(),
   ],
-})
+  build: {
+    rollupOptions: {
+      input: htmlFiles,
+    },
+  },
+});
